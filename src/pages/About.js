b79f1ad14a0f0f86317913c920f0ecfea94d3a9d
Moral <moral@meshkot.com>
2023-02-13 23:12:12 +0000

    added README and updated contact page

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,10 +7,10 @@ const About = () => {
                 width={171}
                 height={180}
                 alt="171x180"
-                src="holder.js/171x180"
+                
             />
             <Figure.Caption>
-                Nulla vitae elit libero, a pharetra augue mollis interdum.
+               profile photo
             </Figure.Caption>
         </Figure>
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,12 @@
 function Contact() {
-    return(<main className="container">
+    return (<main className="container">
         <h2 className="title">Contact</h2>
+        <p>
+            Feel free to email me with any questions at example@gmail.com
+        </p>
+        <p>
+            For any general questions about the layout please see react documentation as well.
+        </p>
     </main>)
 }
 
