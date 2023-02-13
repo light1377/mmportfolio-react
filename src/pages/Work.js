8221e76ade990bb044fe28a393aa819262f22ae2
Moral <moral@meshkot.com>
2023-02-13 23:39:41 +0000

    added projects to work page

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,7 +1,45 @@
+import Table from 'react-bootstrap/Table';
+
+
 function Work() {
-    return(<main className="container">
-        <h2 className="title">Work</h2>
-    </main>)
+    return (<Table striped bordered hover>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Link</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <tr>
+                <td>Basic Profile page</td>
+                <td>https://light1377.github.io/Assignment2-Portfolio/</td>
+
+            </tr>
+            <tr>
+                <td>Console finances</td>
+                <td>"https://light1377.github.io/Console-Finances/</td>
+
+            </tr>
+            <tr>
+                <td>README generator</td>
+                <td>"https://light1377.github.io/Assignment11-READMEgenerator/</td>
+
+            </tr>
+
+        </tbody>
+    </Table>
+
+
+
+
+
+
+
+
+
+
+    );
 }
 
 export default Work;
